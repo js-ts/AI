c98726db32efdd5b85640ef032d2a83e1a96828f
lyuwenyu <wenyu.lyu@gmail.com>
2020-01-17 10:12:44 +0800

    update nn_python_from_scratch

--- lecture01/nn_python_from_scratch.py
+++ lecture01/nn_python_from_scratch.py
@@ -7,18 +7,24 @@ class NeuralNetwork(object):
         self.layers = layers
         self.activations = activations
 
-        self.weights = []
-        self.biases = []
-        for i in range(len(layers) - 1):
-            self.weights.append(np.random.randn(layers[i+1], layers[i]))
-            self.biases.append(np.random.randn(layers[i+1], 1))
-
-        self._weights_grad = [0 for _ in range(len(layers)-1)]
-        self._biases_grad = [0 for _ in range(len(layers)-1)]
+        self.reset_wb()
+        self.reset_wb_grad()
+        # self._weights_grad = [0 for _ in range(len(layers)-1)]
+        # self._biases_grad = [0 for _ in range(len(layers)-1)]
         # self._z_s = []
         # self._a_s = []
 
-    def init_wb_grad(self, ):
+    
+    def reset_wb(self, ):
+        '''
+        '''
+        self.weights = []
+        self.biases = []
+        for i in range(len(self.layers) - 1):
+            self.weights.append(np.random.randn(self.layers[i+1], self.layers[i]))
+            self.biases.append(np.random.randn(self.layers[i+1], 1))
+
+    def reset_wb_grad(self, ):
         '''
         '''
         self._weights_grad = [0 for _ in range(len(self.layers)-1)]
@@ -51,7 +57,7 @@ class NeuralNetwork(object):
         deltas = [None for _ in range(len(self.weights))]  # error delta = dC/dZ
 
         # loss: y - a_s[-1]
-        deltas[-1] = ((y - self._a_s[-1]) * self.getDerivitiveActivationFunc(self.activations[-1])(self._z_s[-1]))
+        deltas[-1] = (y - self._a_s[-1]) * self.getDerivitiveActivationFunc(self.activations[-1])(self._z_s[-1])
 
         for i in reversed(range(len(deltas)-1)):
             deltas[i] = self.weights[i+1].T.dot(deltas[i+1]) * self.getDerivitiveActivationFunc(self.activations[i])(self._z_s[i])
@@ -73,7 +79,7 @@ class NeuralNetwork(object):
         self.biases = [b + lr * db for b, db in zip(self.biases, self._biases_grad)]
         # self._weights_grad = [0 for _ in range(len(self.layers)-1)]
         # self._biases_grad = [0 for _ in range(len(self.layers)-1)]
-
+        self.reset_wb_grad()
 
     @staticmethod
     def get_ActivationFunc(name):
@@ -87,7 +93,7 @@ class NeuralNetwork(object):
                 y[y<0] = 0
                 return y 
             return relu
-        elif name == 'linear':
+        elif name == 'identity':
             return lambda x: x
         else:
             pass
@@ -99,7 +105,7 @@ class NeuralNetwork(object):
         if name == 'sigmoid':
             sig = lambda x: np.exp(x) / (1 + np.exp(x))
             return lambda x: sig(x) * (1 - sig(x))
-        elif name == 'linear':
+        elif name == 'identity':
             return lambda x: 1
         elif name == 'relu':
             def relu_diff(x):
@@ -118,11 +124,10 @@ class NeuralNetwork(object):
                 x_batch = x[:, i: i + batch_size]
                 y_batch = y[:, i: i + batch_size]
                 i += batch_size
+
                 _, a_s = self.feedforward(x_batch)
-                dws, dbs = self.backpropagation(y_batch)
-                # self.update(lr=lr)
-                self.weights = [w + lr * dw for w, dw in zip(self.weights, dws)]
-                self.biases = [b + lr * db for b, db in zip(self.biases, dbs)]
+                self.backpropagation(y_batch)
+                self.update(lr=lr)
 
                 print("loss={}".format(np.linalg.norm(a_s[-1] - y_batch)))
 
@@ -130,14 +135,13 @@ class NeuralNetwork(object):
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
-    nn = NeuralNetwork([1, 100, 1], activations=['sigmoid', 'sigmoid'])
+    nn = NeuralNetwork([1, 32, 32, 1], activations=['sigmoid', 'sigmoid', 'identity'])
     X = 2 * np.pi * np.random.rand(1000).reshape(1, -1)
     y = np.sin(X)
 
-    nn.train(X, y, 10000, 64, 0.1)
+    nn.train(X, y, 3000, 64, 0.1)
 
     _, a_s = nn.feedforward(X)
-    #print(y, X)
     plt.scatter(X.flatten(), y.flatten())
     plt.scatter(X.flatten(), a_s[-1].flatten())
     plt.show()
