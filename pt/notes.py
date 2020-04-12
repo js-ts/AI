60787f75a0c6311c584d91cd6e96f1a3fa0a6608
lyuwenyu <wenyu.lyu@gmail.com>
2020-04-12 15:41:38 +0800

    sth

---
+++ pt/notes.py
@@ -0,0 +1,12 @@
+import random
+import numpy as np
+import torch
+
+
+def set_random(seed_val=0):
+    '''
+    '''
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)
