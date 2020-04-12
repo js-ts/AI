e4ee029847c77a3240a4cd3f86b6053277d9932c
lyuwenyu <wenyu.lyu@gmail.com>
2020-04-12 15:40:48 +0800

    pytorch distributed

--- pt/distributed.py
+++ pt/distributed.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
 
+
+# Parallel
+# Hogwild
+# 
+
+
 # Note: when save distributed train
 # model_to_save = model.module if hasattr(model, 'module') else model
 # 
