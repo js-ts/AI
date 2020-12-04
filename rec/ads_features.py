60ea6fb66307e382011dec62e65f8dce9df88b0e
lyuwenyu <wenyu.lyu@gmail.com>
2020-12-04 15:48:25 +0800

    ads

---
+++ rec/ads_features.py
@@ -0,0 +1,60 @@
+"""Action
+Click
+Impression
+"""
+
+
+"""Ads side features
+ID
+Tile
+Text
+URL
+Advertiser
+Campign
+"""
+
+
+"""User side features
+UID
+Gender
+Age
+Device
+    Type
+    OS/Version
+    IP
+Query
+"""
+
+
+"""Publisher side features
+PublisherID
+"""
+
+
+"""Context features
+Time
+    Date
+    IsWeekDay
+
+Location
+    State
+    Country
+
+"""
+
+
+"""Cross features
+ads cross ads
+ads cross user 
+ads cross publisher
+ads cross context
+
+user cross user
+user cross publisher
+user cross context
+
+publisher cross publisher
+publisher cross context
+
+context cross context
+"""
