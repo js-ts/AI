1fb8d0fb40e79834a7c389bfc57c99537e231a0e
lyuwenyu <wenyu.lyu@gmail.com>
2021-01-05 11:03:06 +0800

    add re

---
+++ utils/test_re.py
@@ -0,0 +1,25 @@
+import re
+
+# Compile
+p = re.compile('[1-9]')
+# p.match
+r = p.search('3213dafd')
+# p.sub
+# p.split
+# p.findall
+# p.finditer
+
+# r.group()
+# r.groups()
+# r.span()
+# r.start()
+# r.end()
+
+
+# Function
+# re.match()
+# re.search()
+# re.sub()
+# re.split()
+# re.finditer()
+# re.finditer()
