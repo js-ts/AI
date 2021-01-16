fdeeb530acf788152b7ee9391ebaa50b3161b604
lyuwenyu <wenyu.lyu@gmail.com>
2021-01-16 13:36:47 +0800

    add test case

---
+++ nn/test_static.py
@@ -0,0 +1,20 @@
+import unittest
+import numpy as np
+
+from static import Tensor
+
+class TestStatic(unittest.TestCase):
+    def test_layer(self, ):
+        
+        pass
+
+        # self.assertEqual()
+
+    @unittest.skip('')
+    def test_tensor(self, ):
+        pass
+
+
+if __name__ == '__main__':
+
+    unittest.main(verbosity=1)
