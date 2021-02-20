63b7b17ead14ad6d90b5893a724e70132c3f75b1
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-20 20:55:21 +0800

    pdll-v1.0

---
+++ nn/pdll-v1.0/nn/modules/padding.py
@@ -0,0 +1,39 @@
+from typing import Union, Tuple
+
+from pdll.autograd import Variable
+
+from ..functional import zero_pad2d, constant_pad2d
+from .module import Module
+
+
+__all__ = [
+    'ZeroPad2d', 'ConstantPad2d', 
+]
+
+class ZeroPad2d(Module):
+    '''ZeroPad2d
+    '''
+    def __init__(self, padding: Union[int, Tuple[int, int, int, int]]):
+        super().__init__()
+        self.padding = padding
+
+    def forward(self, data: Variable) -> Variable:
+        return zero_pad2d(data, padding=self.padding)
+
+    def ext_repr(self, ) -> str:
+        return f'(padding={self.padding})'
+
+
+class ConstantPad2d(Module):
+    '''ConstantPad2d
+    ''' 
+    def __init__(self, padding: Union[int, Tuple[int, int, int, int]], value: float):
+        super().__init__()
+        self.padding = padding
+        self.value = value
+        
+    def forward(self, data: Variable) -> Variable:
+        return constant_pad2d(data, padding=self.padding, value=self.value)
+
+    def ext_repr(self, ) -> str:
+        return f'(padding={self.padding})'
