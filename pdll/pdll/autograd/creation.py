8a882dcef6e90764178ff9af48b4347383fa4fc1
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-25 13:48:32 +0800

    test backend

--- pdll/pdll/autograd/creation.py
+++ pdll/pdll/autograd/creation.py
@@ -3,6 +3,8 @@ from typing import Tuple, Union
 from copy import deepcopy
 
 from pdll.backend.executor import np, support_types
+from pdll.backend import executor
+
 from .tensor import Tensor
 
 
@@ -10,18 +12,18 @@ __all__ = [
     'rand', 'randn', 'eye',
     'ones', 'ones_like',
     'zeros', 'zeros_like',
-    'from_numpy', 'tensor',
+    'from_numpy',
 ]
 
 
-def tensor(data: Union[support_types]) -> Tensor:
-    return Tensor(deepcopy(data))
+# def tensor(data: Union[support_types]) -> Tensor:
+#     return Tensor(deepcopy(data))
 
 
 def rand(*shape: Tuple[int], requires_grad=False) -> Tensor:
     '''
     '''
-    data = np.random.rand(*shape)
+    data = executor.np.random.rand(*shape)
     return Tensor(data, requires_grad=requires_grad)
 
 
