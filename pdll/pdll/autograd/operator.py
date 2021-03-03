update var-sample variance, mean. and fixed bug in grad.reshape

pdll/pdll/autograd/operator.py
@@ -198,14 +198,11 @@ class _Mean(Function):
         if axis is None:
             axis = tuple(range(len(self.t_shape)))
 
-        if self.keepdims:
-            shape = grad.shape
-        else:
-            shape = [(1 if i in axis else d) for i, d in enumerate(self.t_shape)]
+        _shape = [(1 if i in axis else d) for i, d in enumerate(self.t_shape)]
 
         ks = [self.t_shape[i] for i in axis] if axis else [1]
 
-        return grad.reshape(shape) * executor.np.ones(self.t_shape) / REDUCE(MUL, ks)
+        return grad.reshape(_shape) * executor.np.ones(self.t_shape) / REDUCE(MUL, ks)
 
 
 class _Pow(Function):
@@ -313,13 +310,11 @@ class _Var(Function):
         if self.axis is None:
             _axis = tuple(range(len(self.t_shape)))
         
-        if self.keepdims:
-            _shape = self.t_shape
-        else:
-            _shape = [(1 if i in _axis else d) for i, d in enumerate(self.t_shape)]
+        _shape = [(1 if i in _axis else d) for i, d in enumerate(self.t_shape)]
         
         grad = grad.reshape(*_shape)
 
+        # return grad * 2 / self.n * self.t_minus_mean
         return grad * 2 / (self.n - 1) * self.t_minus_mean
 
 

pdll/pdll/nn/modules/normalization.py
@@ -70,7 +70,7 @@ class BatchNorm2d(Module):
         if self.training:
             mean = data.mean(axis=(0, 2, 3), keepdims=True)
             var = ((data - mean) ** 2).mean(axis=(0, 2, 3), keepdims=True)
-
+            # var = data.var(axis=(0, 2, 3), keepdims=True)
         else:
             mean = self.running_mean.reshape(1, -1, 1, 1)
             var = self.running_var.reshape(1, -1, 1, 1)