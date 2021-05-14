6cc5c228cd2ee7ad47dc5725a9edd69bfefc1e07
lyuwenyu <wenyu.lyu@gmail.com>
2021-05-14 14:34:41 +0800

    monodepth

---
+++ 3d/monodepth/model/base_resnet.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+import numpy as np 
+
+
+class ResnetBase(nn.Module):
+    """
+    """
+    def __init__(self, name, in_channels=3, num_layers=4, pretrained=True, ):
+        super().__init__()
+
+        net = getattr(models, name)(pretrained)
+        net.conv1 = nn.Conv2d(in_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        net = nn.Sequential(*list(net.children())[:-2]) 
+
+        self.net = net
+        self.num_layers = num_layers
+        self.output_channels = np.array([64, 128, 256, 512])
+        
+    def forward(self, data):
+        outputs = []
+        for m in self.net.children():
+            data = m(data)
+            outputs.append(data)
+
+        return outputs[-self.num_layers:]
+
+
+
+if __name__ == '__main__':
+
+    mm = ResnetBase('resnet50', 3, 4, True)
+
+
+    data = torch.rand(1, 3, 120, 120)
+    outs = mm(data)
+
+    for out in outs:
+        print(out.shape)
+
